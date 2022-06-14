b2ccde53552f3c299f8d92e20afcd070d5b0a1e8
Kimiya <kimiatavakol9@gmail.com>
2022-06-15 00:19:01 +0430

    Add Gauge Chart

---
+++ src/components/chart/gaugeChart.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Chart from "react-apexcharts";
+
+class GaugeChart extends Component {
+    
+    state = {
+        series: [70],
+            options: {
+              chart: {
+                type: 'radialBar',
+              },
+              plotOptions: {
+                radialBar: {
+                  fill: "#fc0",
+                  hollow: {
+                    size: '70%',
+                  }
+                },
+              },
+              fill: {
+                colors: ['#fc0']
+              },
+              labels: ['6000 تومان'],
+            },
+          
+          
+          };
+  
+    render() {
+  
+      return (
+        <div className="radialBar">
+          <Chart options={this.state.options} series={this.state.series} type="radialBar"/>
+        </div>
+      );
+    }
+}
+ 
+export default GaugeChart;
